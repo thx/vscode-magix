932e1e96f28f3606000091f0a3c3c328f73d9b02
付英军 <fuyingjun@alibaba-inc.com>
2019-10-28 12:09:22 +0800

    fix:修正code 取值范围问题

--- src/provider/IconfontHoverProvider.ts
+++ src/provider/IconfontHoverProvider.ts
@@ -8,12 +8,12 @@ export class IconfontHoverProvider implements vscode.HoverProvider {
 
     const line = document.lineAt(position.line).text;
 
-    //鼠标悬浮位置左右个8个字符。保证能圈到<i> innerHTML 内容
+    //鼠标悬浮位置左右个8个字符。
     let start: number = position.character < 8 ? 0 : position.character - 8;
-    let end: number = position.character > line.length - 8 ? line.length - 1 : position.character + 8;
+    let end: number = position.character > line.length - 8 ? line.length : position.character + 8;
 
     let rangTxt = line.substring(start, end);
-
+ 
     rangTxt.match(/\&\#x(\w+);/);
     let code: string = RegExp.$1;
 
